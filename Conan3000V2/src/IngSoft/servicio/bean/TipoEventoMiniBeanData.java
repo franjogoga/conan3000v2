9895a07a17fd91aac8c0403547d9cfaaa449dfe7
victor.avalos.91@gmail.com <victor.avalos.91@gmail.com@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-05-02 16:40:15 +0000

    VA_pruebas mybatis-Victor Avalos

---
+++ Conan3000V2/src/IngSoft/servicio/bean/TipoEventoMiniBeanData.java
@@ -0,0 +1,20 @@
+package IngSoft.servicio.bean;
+
+public class TipoEventoMiniBeanData {
+	private int codigo;
+	private String nombre;
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+
+}
