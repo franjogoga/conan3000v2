6ba8c373939ad733c8c6d1ec612169a862d25fe8
cbustamanteaponte@gmail.com <cbustamanteaponte@gmail.com@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-05-05 20:20:07 +0000

    ati

--- trunk/Conan3000V2/src/IngSoft/servicio/sorteo/AccionBuscarSorteo.java
+++ trunk/Conan3000V2/src/IngSoft/servicio/sorteo/AccionBuscarSorteo.java
@@ -1,5 +1,29 @@
 package IngSoft.servicio.sorteo;
 
-public class AccionBuscarSorteo {
+import java.util.Vector;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import IngSoft.general.CoAccion;
+import IngSoft.general.CoException;
+import IngSoft.servicio.bean.CriterioSorteoBeanData;
+import IngSoft.servicio.bean.CriterioSorteoBeanFuncion;
+import IngSoft.servicio.bean.ResultadoSorteoBeanData;
+
+public class AccionBuscarSorteo extends CoAccion {
+
+	@Override
+	public void ejecutar(ServletContext sc, HttpServletRequest request,
+			HttpServletResponse response) throws CoException {
+		// TODO Auto-generated method stub
+		CriterioSorteoBeanData criterioSorteoData =new CriterioSorteoBeanFuncion().crearCriterio(request,response);
+		Vector<ResultadoSorteoBeanData> resultados=new CriterioSorteoBeanFuncion().buscarPlantillaSorteo(criterioSorteoData);
+
+		request.setAttribute("resultados", resultados);
+		this.direccionar(sc, request, response, "/IngSoft/servicio/evento/buscarevento.jsp");
+		
+	}
 
 }
