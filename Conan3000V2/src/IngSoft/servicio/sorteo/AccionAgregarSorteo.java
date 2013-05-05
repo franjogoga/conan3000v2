58407683917bfe5421914d820b3d776281f9a634
cbustamanteaponte@gmail.com <cbustamanteaponte@gmail.com@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-05-05 20:20:07 +0000

    ati

--- Conan3000V2/src/IngSoft/servicio/sorteo/AccionAgregarSorteo.java
+++ Conan3000V2/src/IngSoft/servicio/sorteo/AccionAgregarSorteo.java
@@ -1,19 +1,41 @@
 package IngSoft.servicio.sorteo;
 
+import java.util.Vector;
+
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import IngSoft.general.CoAccion;
 import IngSoft.general.CoException;
+import IngSoft.servicio.bean.AmbienteMiniBeanData;
+import IngSoft.servicio.bean.EventoBeanData;
+import IngSoft.servicio.bean.EventoBeanFuncion;
+import IngSoft.servicio.bean.SedeMiniBeanData;
+import IngSoft.servicio.bean.SorteoBeanFuncion;
+import IngSoft.servicio.bean.TipoEventoMiniBeanData;
 
 public class AccionAgregarSorteo extends CoAccion{
 
 	@Override
-	public void ejecutar(ServletContext arg0, HttpServletRequest arg1,
-			HttpServletResponse arg2) throws CoException {
+	public void ejecutar(ServletContext sc, HttpServletRequest request,
+			HttpServletResponse response) throws CoException {
 		// TODO Auto-generated method stub
-		
+		int tipo=Integer.parseInt(request.getParameter("tipo"));
+		if(tipo==2){
+			EventoBeanFuncion eventoFuncion= EventoBeanFuncion.getInstance();
+			EventoBeanData eventoData=eventoFuncion.crearEvento(request, response);
+			eventoFuncion.agregarEvento(eventoData);
+			this.direccionar(sc, request, response, "/IngSoft/servicio/evento/buscarsorteo.jsp");
+		}
+		SorteoBeanFuncion sorteoFunction= SorteoBeanFuncion.getInstance();
+		Vector<SedeMiniBeanData> sedeMiniData=sorteoFunction.getSedes();
+		Vector<TipoEventoMiniBeanData> tipoEventoMiniData=sorteoFunction.getTipoEvento();
+		Vector<AmbienteMiniBeanData> AmbienteMiniData=sorteoFunction.getAmbientes();
+		request.setAttribute("sedes",sedeMiniData);
+		request.setAttribute("ambientes",AmbienteMiniData );
+		request.setAttribute("tiposEvento",tipoEventoMiniData);
+		this.direccionar(sc, request, response, "/IngSoft/servicio/evento/agregarsorteo.jsp");
 	}
 
 }
