7291bd2492ba88e1a464dfc3ed9648ed76d55d41
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-05-19 17:43:55 +0000

    DB_creacion de SocioBeanData

---
+++ trunk/Conan3000V2/src/IngSoft/venta/bean/SocioBeanData.java
@@ -0,0 +1,128 @@
+package IngSoft.venta.bean;
+
+import java.io.Serializable;
+
+public class SocioBeanData implements Serializable{
+
+	private String codigo;
+	private String nombres;
+	private String apellidoPaterno;
+	private String apellidoMaterno;
+	private String fechaNacimiento;
+	private String tipoDocumento;
+	private int numeroDocumento;
+	private String correoElectronico;
+	private String direccion;
+	private long telefonoFijo;
+	private long telefonoCelular;
+	private String idDistrito;
+	private String idPerfil;
+	
+	
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	
+	public String getNombres() {
+		return nombres;
+	}
+	
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	
+	public String getApellidoPaterno() {
+		return apellidoPaterno;
+	}
+	
+	public void setApellidoPaterno(String apellidoPaterno) {
+		this.apellidoPaterno = apellidoPaterno;
+	}
+	
+	public String getApellidoMaterno() {
+		return apellidoMaterno;
+	}
+	
+	public void setApellidoMaterno(String apellidoMaterno) {
+		this.apellidoMaterno = apellidoMaterno;
+	}
+	
+	public String getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	
+	public void setFechaNacimiento(String fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	
+	public String getTipoDocumento() {
+		return tipoDocumento;
+	}
+	
+	public void setTipoDocumento(String tipoDocumento) {
+		this.tipoDocumento = tipoDocumento;
+	}
+	
+	public int getNumeroDocumento() {
+		return numeroDocumento;
+	}
+	
+	public void setNumeroDocumento(int numeroDocumento) {
+		this.numeroDocumento = numeroDocumento;
+	}
+	
+	public String getCorreoElectronico() {
+		return correoElectronico;
+	}
+	
+	public void setCorreoElectronico(String correoElectronico) {
+		this.correoElectronico = correoElectronico;
+	}
+	
+	public String getDireccion() {
+		return direccion;
+	}
+	
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+	public long getTelefonoFijo() {
+		return telefonoFijo;
+	}
+	
+	public void setTelefonoFijo(long telefonoFijo) {
+		this.telefonoFijo = telefonoFijo;
+	}
+	
+	public long getTelefonoCelular() {
+		return telefonoCelular;
+	}
+	
+	public void setTelefonoCelular(long telefonoCelular) {
+		this.telefonoCelular = telefonoCelular;
+	}
+
+	public String getIdDistrito() {
+		return idDistrito;
+	}
+
+	public void setIdDistrito(String idDistrito) {
+		this.idDistrito = idDistrito;
+	}
+
+	public String getIdPerfil() {
+		return idPerfil;
+	}
+
+	public void setIdPerfil(String idPerfil) {
+		this.idPerfil = idPerfil;
+	}
+	
+	
+}
