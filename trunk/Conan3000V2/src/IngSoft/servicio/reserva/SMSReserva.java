3737b2812d4229755884ff624f7244a140669088
victor.avalos.91@gmail.com <victor.avalos.91@gmail.com@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-05-21 05:02:18 +0000

    VA_:....:-Victor Avalos

---
+++ trunk/Conan3000V2/src/IngSoft/servicio/reserva/SMSReserva.java
@@ -0,0 +1,17 @@
+   package IngSoft.servicio.reserva;
+
+import IngSoft.general.CoServlet;
+
+public class SMSReserva extends CoServlet{
+	
+	/**
+	 * 
+	 */
+//	private static final long serialVersionUID = -8594038457439090712L;
+
+	@Override
+	protected void initAcciones() {
+		acciones.put("Buscar", new AccionBuscarReserva());		
+	}
+	
+}
