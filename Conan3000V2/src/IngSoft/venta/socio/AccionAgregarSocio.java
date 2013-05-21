576fd7064f97ee27e5efe4f6d4a1bb141b68b9d0
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-05-21 04:00:41 +0000

    DB_final de AccionAgregarSocio

--- Conan3000V2/src/IngSoft/venta/socio/AccionAgregarSocio.java
+++ Conan3000V2/src/IngSoft/venta/socio/AccionAgregarSocio.java
@@ -1,11 +1,14 @@
 package IngSoft.venta.socio;
 
+import java.util.Vector;
+
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import IngSoft.general.CoAccion;
 import IngSoft.general.CoException;
+import IngSoft.venta.bean.DistritoMiniBeanData;
 import IngSoft.venta.bean.SocioBeanData;
 import IngSoft.venta.bean.SocioBeanFuncion;
 
@@ -21,8 +24,25 @@ public class AccionAgregarSocio extends CoAccion{
 					SocioBeanFuncion socioFuncion= SocioBeanFuncion.getInstance();
 					SocioBeanData socioData=socioFuncion.crearSocio(request, response);
 					socioFuncion.agregarSocio(socioData);
+					
+					String codigo=socioData.getCodigo();
+					
+					
+					//aca agregar el codigo para que se pase el codigo que se ha generado
+					
 					this.direccionar(sc, request, response, "/IngSoft/ventas/promociones/trasladomembresia.jsp");
+					//request.setAttribute("typeahead7",codigo);
+				
 				}
+				SocioBeanFuncion socioFunction= SocioBeanFuncion.getInstance();
+				Vector<DistritoMiniBeanData> distritoMiniData=socioFunction.getDistritos();
+				//Vector<TipoEventoMiniBeanData> tipoEventoMiniData=eventoFunction.getTipoEvento();
+				//Vector<AmbienteMiniBeanData> AmbienteMiniData=eventoFunction.getAmbientes();
+				//request.setAttribute("sedes",sedeMiniData );
+				//request.setAttribute("ambientes",AmbienteMiniData );
+				
+				
+				request.setAttribute("tiposDistrito",distritoMiniData);
 				
 				this.direccionar(sc, request, response, "/IngSoft/ventas/socio/agregarsocio.jsp");
 				}
