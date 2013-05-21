2f37c60a73a0dc531ea7cc198a06cf9917bc11d0
victor.avalos.91@gmail.com <victor.avalos.91@gmail.com@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-05-21 05:02:18 +0000

    VA_:....:-Victor Avalos

---
+++ Conan3000V2/src/IngSoft/servicio/bean/ReservaBeanFuncion.java
@@ -0,0 +1,27 @@
+package IngSoft.servicio.bean;
+
+import java.util.List;
+import java.util.Vector;
+
+import org.apache.ibatis.session.SqlSession;
+
+import IngSoft.general.MyBatisSesion;
+
+public class ReservaBeanFuncion {
+	static private ReservaBeanFuncion ReservaFuncion=null;
+	
+	 public static ReservaBeanFuncion getInstance(){
+	       if(ReservaFuncion==null) ReservaFuncion= new ReservaBeanFuncion();
+	       
+	       return ReservaFuncion;
+	   }
+	   
+	   private ReservaBeanFuncion() {}
+	   
+	   public Vector<SedeMiniBeanData> getSedes(){
+			SqlSession sqlsesion=MyBatisSesion.metodo().openSession();
+			List<SedeMiniBeanData> resultados=sqlsesion.selectList("Data.servicio.evento.searchSedeMini");
+			sqlsesion.close();
+			return new Vector<>(resultados);
+		}  
+}
---
+++ Conan3000V2/src/IngSoft/servicio/reserva/AccionBuscarReserva.java
@@ -0,0 +1,36 @@
+package IngSoft.servicio.reserva;
+
+import java.util.Vector;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import IngSoft.general.CoAccion;
+import IngSoft.general.CoException;
+import IngSoft.servicio.bean.CriterioEventoBeanData;
+import IngSoft.servicio.bean.CriterioEventoBeanFunction;
+import IngSoft.servicio.bean.ReservaBeanFuncion;
+import IngSoft.servicio.bean.ResultadoEventoBeanData;
+import IngSoft.servicio.bean.SedeMiniBeanData;
+
+public class AccionBuscarReserva extends CoAccion {
+
+	@Override
+	public void ejecutar(ServletContext sc, HttpServletRequest request,
+			HttpServletResponse response) throws CoException {
+		int tipo=Integer.parseInt(request.getParameter("tipo"));
+		if(tipo==2){
+			if("bungalow".equals(request.getParameter("cmbServicios"))) 
+				this.direccionar(sc, request, response, "/IngSoft/servicio/reserva/reservaxdia.jsp");
+			else this.direccionar(sc, request, response, "/IngSoft/servicio/reserva/reservaxhora.jsp");
+			
+		}
+		ReservaBeanFuncion reservaFuncion= ReservaBeanFuncion.getInstance();
+		Vector<SedeMiniBeanData> sedeMiniData=reservaFuncion.getSedes();
+		request.setAttribute("sedes",sedeMiniData );
+		//request.setAttribute("resultados", resultados);
+		this.direccionar(sc, request, response, "/IngSoft/servicio/reserva/buscarreserva.jsp");
+	}
+
+}
