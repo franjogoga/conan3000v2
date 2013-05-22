a21ff165f67f958d644161a190f5d5c908852398
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-05-22 07:37:55 +0000

    DB_medio SMVSocio

--- trunk/Conan3000V2/src/IngSoft/venta/socio/SMVSocio.java
+++ trunk/Conan3000V2/src/IngSoft/venta/socio/SMVSocio.java
@@ -1,17 +1,17 @@
 package IngSoft.venta.socio;
 
 import IngSoft.general.CoServlet;
-import IngSoft.venta.promocion.AccionAgregarPromocion;
-import IngSoft.venta.promocion.AccionBuscarPromocion;
-import IngSoft.venta.promocion.AccionConsultarPromocion;
-import IngSoft.venta.promocion.AccionEliminarPromocion;
-import IngSoft.venta.promocion.AccionModificarPromocion;
+import IngSoft.venta.socio.AccionAgregarSocio;
+import IngSoft.venta.socio.AccionBuscarSocio;
+//import IngSoft.venta.socio.AccionConsultarSocio;
+//import IngSoft.venta.socio.AccionEliminarSocio;
+//import IngSoft.venta.socio.AccionModificarSocio;
 
 public class SMVSocio extends CoServlet{
 	
 	@Override
 	protected void initAcciones() {
-		//acciones.put("Buscar", new AccionBuscarPromocion());
+		acciones.put("Buscar", new AccionBuscarSocio());
 		//acciones.put("Consultar", new AccionConsultarPromocion());
 		acciones.put("Agregar", new AccionAgregarSocio());
 		//acciones.put("Modificar", new AccionModificarPromocion());
