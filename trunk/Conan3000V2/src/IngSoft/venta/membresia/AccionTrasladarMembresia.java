e6bb560ac09c75e43374d2cb33a117290c951838
lizita.diaz@gmail.com <lizita.diaz@gmail.com@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-05-23 07:47:51 +0000

    LD

---
+++ trunk/Conan3000V2/src/IngSoft/venta/membresia/AccionTrasladarMembresia.java
@@ -0,0 +1,40 @@
+package IngSoft.venta.membresia;
+
+
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import IngSoft.general.CoAccion;
+import IngSoft.general.CoException;
+
+import IngSoft.venta.bean.MembresiaBeanData;
+import IngSoft.venta.bean.MembresiaBeanFunction;
+
+ public class AccionTrasladarMembresia  extends CoAccion {
+
+	@Override
+	public void ejecutar(ServletContext sc, HttpServletRequest request,
+			HttpServletResponse response) throws CoException {
+		// TODO Auto-generated method stub
+		
+		if(Integer.valueOf(request.getParameter("tipo"))==2){
+			
+			MembresiaBeanFunction membresiaFuncion= MembresiaBeanFunction.getInstance();
+			membresiaFuncion.eliminarMembresia(request.getParameter("codigo"));
+			this.direccionar(sc, request, response, "/IngSoft/ventas/membresia/buscarmembresia.jsp");
+		}
+		if(Integer.valueOf(request.getParameter("tipo"))==1){
+		MembresiaBeanFunction membresiaFuncion= MembresiaBeanFunction.getInstance();
+		MembresiaBeanData membresiaData=membresiaFuncion.consultarMembresia(request.getParameter("codigo"));
+		
+		request.setAttribute("membresia", membresiaData);
+		this.direccionar(sc, request, response, "/IngSoft/ventas/membresia/trasladomembresia.jsp");
+		}
+		//this.direccionar(sc, request, response, "/IngSoft/ventas/membresiaes/buscarmembresia.jsp");
+					
+				
+	}
+
+}
