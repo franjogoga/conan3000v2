1440528f16fb625f60d8def7c827369a29247c8c
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-05-23 06:14:35 +0000

    DB_nuevo criterioSocioBeandata

---
+++ trunk/Conan3000V2/src/IngSoft/venta/bean/CriterioSocioBeanData.java
@@ -0,0 +1,77 @@
+package IngSoft.venta.bean;
+
+import java.io.Serializable;
+
+public class CriterioSocioBeanData implements Serializable{
+
+	private String idSocio;
+	private String nombres;
+	private String apellidoPaterno;
+	private String apellidoMaterno;
+	private String direccion;
+	private String tipoDocumento;
+	private long numeroDocumento;
+	
+	
+	
+	
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getIdSocio() {
+		return idSocio;
+	}
+	
+	public void setIdSocio(String idSocio) {
+		this.idSocio = idSocio;
+	}
+	
+	public String getApellidoPaterno() {
+		return apellidoPaterno;
+	}
+	
+	public void setApellidoPaterno(String apellidoPaterno) {
+		this.apellidoPaterno = apellidoPaterno;
+	}
+	
+	public String getApellidoMaterno() {
+		return apellidoMaterno;
+	}
+	
+	public void setApellidoMaterno(String apellidoMaterno) {
+		this.apellidoMaterno = apellidoMaterno;
+	}
+	
+	public String getTipoDocumento() {
+		return tipoDocumento;
+	}
+	
+	public void setTipoDocumento(String tipoDocumento) {
+		this.tipoDocumento = tipoDocumento;
+	}
+	
+	public long getNumeroDocumento() {
+		return numeroDocumento;
+	}
+	
+	public void setNumeroDocumento(long numeroDocumento) {
+		this.numeroDocumento = numeroDocumento;
+	}
+	
+	
+	
+	
+}
