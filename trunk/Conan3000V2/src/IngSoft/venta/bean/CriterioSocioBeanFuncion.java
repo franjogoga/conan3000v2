a7a2f552318dfc75e13a3f66ffd3d18baaa742fb
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-05-23 06:14:20 +0000

    DB_nuevo criterioSocioBeanFuncion

---
+++ trunk/Conan3000V2/src/IngSoft/venta/bean/CriterioSocioBeanFuncion.java
@@ -0,0 +1,59 @@
+package IngSoft.venta.bean;
+
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Vector;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.ibatis.session.SqlSession;
+
+import IngSoft.general.MyBatisSesion;
+
+public class CriterioSocioBeanFuncion {
+
+	SimpleDateFormat DF = new SimpleDateFormat("dd/MM/yyyy");
+
+	public CriterioSocioBeanData crearCriterio(HttpServletRequest request, HttpServletResponse response){
+		
+		CriterioSocioBeanData criterioSocioData= new CriterioSocioBeanData();
+		
+		//criterioPromocionData.setTipo(Integer.parseInt(request.getParameter("cmbTipoEvento")==null?"0":request.getParameter("cmbTipoEvento")));
+		criterioSocioData.setNombres(request.getParameter("txtNombres")+"%");
+		criterioSocioData.setApellidoMaterno(request.getParameter("txtApellidoMaterno")+"%");
+		criterioSocioData.setApellidoPaterno(request.getParameter("txtApellidoPaterno")+"%");
+		
+		criterioSocioData.setTipoDocumento(request.getParameter("cmdTipoDocumento"));
+		
+		//try {		
+			//criterioSocioData.setNumeroDocumento(Long.parseLong(request.getParameter("txtNumeroDocumento")));
+			//if (request.getParameter("rButton")!=null){
+			//String est;
+			//est=request.getParameter("rButton");
+			//criterioPromocionData.setEstado(est);}
+
+		//criterioPromocionData.setTipo(Integer.parseInt(request.getParameter("cmbTipoEvento")==null?"0":request.getParameter("cmbTipoEvento")));
+
+		//} catch (ParseException e) {
+			//e.printStackTrace();
+		//}
+		return criterioSocioData;				
+	}
+	
+	public Vector<ResultadoSocioBeanData> buscarPlantillaSocio(CriterioSocioBeanData criterioSocioData){		
+		SqlSession sqlsesion=MyBatisSesion.metodo().openSession();
+		Vector<ResultadoSocioBeanData> resultadosV=null;
+		try{		
+		List<ResultadoSocioBeanData> resultados=sqlsesion.selectList("searchPlantillaSocio",criterioSocioData);
+						
+		resultadosV= new Vector<>(resultados);
+		}
+		finally{
+		sqlsesion.close();}
+		return resultadosV;
+		
+	}
+}
