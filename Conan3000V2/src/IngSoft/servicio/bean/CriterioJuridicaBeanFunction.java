21354e003bd7b675447fb8a9b6e572ebaa1cb4b8
david.bigio@pucp.pe <david.bigio@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-05-24 05:38:51 +0000

    prueba

---
+++ Conan3000V2/src/IngSoft/servicio/bean/CriterioJuridicaBeanFunction.java
@@ -0,0 +1,44 @@
+package IngSoft.servicio.bean;
+
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.ibatis.session.SqlSession;
+
+import IngSoft.general.MyBatisSesion;
+
+public class CriterioJuridicaBeanFunction {
+	SimpleDateFormat DF = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public PersonaJuridicaBeanData crearCriterio(HttpServletRequest request, HttpServletResponse response){
+		PersonaJuridicaBeanData juridicaData= new PersonaJuridicaBeanData();
+		
+		juridicaData.setRazonSocial(request.getParameter("txtRazonSocial").trim()+"%");
+		juridicaData.setRuc(Integer.parseInt(request.getParameter("txtRuc")));
+		juridicaData.setEstado("Activo");
+		
+		return juridicaData;				
+	}
+	
+	public Vector<ResultadoJuridicaBeanData> buscarPlantillaJuridica(PersonaJuridicaBeanData juridicaData){		
+		SqlSession sqlsesion=MyBatisSesion.metodo().openSession();
+		Vector<ResultadoJuridicaBeanData> resultadosV=null;
+		try{		
+		List<ResultadoJuridicaBeanData> resultados = sqlsesion.selectList("searchPlantilla",juridicaData);
+	
+		resultadosV= new Vector<>(resultados);
+		}
+		finally{
+		sqlsesion.close();}
+		return resultadosV;
+		
+	}
+
+}
