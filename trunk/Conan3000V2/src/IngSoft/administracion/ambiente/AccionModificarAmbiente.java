b6afcce22f613b1eed5c20011e8dd39764f30798
luis.alvarado@pucp.pe <luis.alvarado@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-05-26 05:17:23 +0000

    LA - mantenimiento ambiente

--- trunk/Conan3000V2/src/IngSoft/administracion/ambiente/AccionModificarAmbiente.java
+++ trunk/Conan3000V2/src/IngSoft/administracion/ambiente/AccionModificarAmbiente.java
@@ -1,18 +1,14 @@
 package IngSoft.administracion.ambiente;
 
-import java.util.Vector;
-
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import IngSoft.general.CoAccion;
 import IngSoft.general.CoException;
-import IngSoft.servicio.bean.AmbienteMiniBeanData;
-import IngSoft.servicio.bean.EventoBeanData;
-import IngSoft.servicio.bean.EventoBeanFuncion;
-import IngSoft.servicio.bean.SedeMiniBeanData;
-import IngSoft.servicio.bean.TipoEventoMiniBeanData;
+import IngSoft.administracion.bean.AmbienteBeanData;
+import IngSoft.administracion.bean.AmbienteBeanFunction;
+import IngSoft.administracion.bean.PerfilBeanData;
 
 public class AccionModificarAmbiente extends CoAccion {
 
@@ -20,27 +16,15 @@ public class AccionModificarAmbiente extends CoAccion {
 	public void ejecutar(ServletContext sc, HttpServletRequest request,
 			HttpServletResponse response) throws CoException{
 		// TODO Auto-generated method stub
-		EventoBeanFuncion eventoFuncion= EventoBeanFuncion.getInstance();
+		AmbienteBeanFunction ambienteFunction= AmbienteBeanFunction.getInstance();
 		if(Integer.valueOf(request.getParameter("tipo"))==2){
-			//System.out.println(request.getParameter("txtLSede"));
-			//System.out.println(request.getParameter("txtLAmbiente"));
-			
-			String antSede[]=((String)request.getParameter("txtLSede")).split("/");
-			String antAmb[]=((String)request.getParameter("txtLAmbiente")).split("/");
-			EventoBeanData eventoData=eventoFuncion.crearEvento(request, response);
-			eventoData.setCodigo(request.getParameter("codigo"));
-			eventoFuncion.modificarEvento(eventoData, antSede, antAmb);
-			this.direccionar(sc, request, response, "/IngSoft/servicio/evento/buscarevento.jsp");
+			AmbienteBeanData ambienteData = ambienteFunction.crearAmbiente(request, response);
+			ambienteData.setCodigo(request.getParameter("codigo"));
+			ambienteFunction.modificarAmbiente(ambienteData);
+			this.direccionar(sc, request, response,"/IngSoft/administracion/ambiente/buscarambiente.jsp");
 		}	
-		EventoBeanData eventoData=eventoFuncion.consultarEvento(request.getParameter("codigo"));
-		Vector<SedeMiniBeanData> sedeMiniData=eventoFuncion.getSedes();
-		Vector<TipoEventoMiniBeanData> tipoEventoMiniData=eventoFuncion.getTipoEvento();
-		Vector<AmbienteMiniBeanData> AmbienteMiniData=eventoFuncion.getAmbientes();
-		request.setAttribute("sedes",sedeMiniData );
-		request.setAttribute("ambientes",AmbienteMiniData );
-		request.setAttribute("tiposEvento",tipoEventoMiniData);
-		request.setAttribute("evento", eventoData);
-		this.direccionar(sc, request, response, "/IngSoft/servicio/evento/modificarevento.jsp");
+		AmbienteBeanData ambienteData = ambienteFunction.consultarAmbiente(request.getParameter("codigo"));
+		request.setAttribute("ambiente", ambienteData);
+		this.direccionar(sc, request, response,"/IngSoft/administrar/ambiente/modificarambiente.jsp");
 	}
-
 }
