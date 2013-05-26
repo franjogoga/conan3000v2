bd092c59ebd74139e2ebe7bde5bc483d7092961d
luis.alvarado@pucp.pe <luis.alvarado@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-05-26 04:09:39 +0000

    LA - mantenimiento ambiente - luis

--- trunk/Conan3000V2/src/IngSoft/administracion/ambiente/SMAAmbiente.java
+++ trunk/Conan3000V2/src/IngSoft/administracion/ambiente/SMAAmbiente.java
@@ -7,9 +7,9 @@ public class SMAAmbiente extends CoServlet{
 	@Override
 	protected void initAcciones() {
 		acciones.put("Buscar", new AccionBuscarAmbiente());
-		//acciones.put("Consultar", new AccionConsultarAmbiente());
+		acciones.put("Consultar", new AccionConsultarAmbiente());
 		acciones.put("Agregar", new AccionAgregarAmbiente());
-		//acciones.put("Modificar", new AccionModificarAmbiente());
-		//acciones.put("Eliminar", new AccionEliminarAmbiente());
+		acciones.put("Modificar", new AccionModificarAmbiente());
+		acciones.put("Eliminar", new AccionEliminarAmbiente());
 	}
 }
