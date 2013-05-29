b5a9c7331e3577e0de6acb4763f9fd0d0b3a14c3
lizita.diaz@gmail.com <lizita.diaz@gmail.com@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-05-29 06:09:54 +0000

    LD

---
+++ Conan3000V2/src/IngSoft/venta/bean/ProveedorBeanData.java
@@ -0,0 +1,130 @@
+package IngSoft.venta.bean;
+
+import java.io.Serializable;
+
+public class ProveedorBeanData implements Serializable {
+	
+	private String idProveedor;
+	
+	private String razonSocial;
+	private long ruc;
+	private String direccion;
+	private long telefono;
+	private String correo;
+	private String nomContacto;
+	private String apPaterno;
+	private String apMaterno;
+	private String estado;
+	private String tipoDoc;
+	private String numDoc;
+	private String distrito;
+	private String provincia;
+	private String departamento;
+	
+	
+	public String getDistrito() {
+		return distrito;
+	}
+	public void setDistrito(String distrito) {
+		this.distrito = distrito;
+	}
+	public String getProvincia() {
+		return provincia;
+	}
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+	public String getDepartamento() {
+		return departamento;
+	}
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+	
+	
+	public String getIdProveedor() {
+		return idProveedor;
+	}
+	public void setIdProveedor(String idProveedor) {
+		this.idProveedor = idProveedor;
+	}
+	public String getRazonSocial() {
+		return razonSocial;
+	}
+	public void setRazonSocial(String razonSocial) {
+		this.razonSocial = razonSocial;
+	}
+	public long getRuc() {
+		return ruc;
+	}
+	public void setRuc(long ruc) {
+		this.ruc = ruc;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public long getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(long telefono) {
+		this.telefono = telefono;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public String getNomContacto() {
+		return nomContacto;
+	}
+	public void setNomContacto(String nomContacto) {
+		this.nomContacto = nomContacto;
+	}
+	public String getApPaterno() {
+		return apPaterno;
+	}
+	public void setApPaterno(String apPaterno) {
+		this.apPaterno = apPaterno;
+	}
+	public String getApMaterno() {
+		return apMaterno;
+	}
+	public void setApMaterno(String apMaterno) {
+		this.apMaterno = apMaterno;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getTipoDoc() {
+		return tipoDoc;
+	}
+	public void setTipoDoc(String tipoDoc) {
+		this.tipoDoc = tipoDoc;
+	}
+	public String getNumDoc() {
+		return numDoc;
+	}
+	public void setNumDoc(String numDoc) {
+		this.numDoc = numDoc;
+	}
+
+	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+
+
