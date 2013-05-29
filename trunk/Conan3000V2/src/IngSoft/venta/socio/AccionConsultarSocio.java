b15c9291614b26e740c5c1295f2fedb85e5539ac
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-05-29 06:03:18 +0000

    DB_AccionConsultarSOcio

---
+++ trunk/Conan3000V2/src/IngSoft/venta/socio/AccionConsultarSocio.java
@@ -0,0 +1,35 @@
+package IngSoft.venta.socio;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import IngSoft.general.CoAccion;
+import IngSoft.general.CoException;
+import IngSoft.venta.bean.PersonaMiniBeanData;
+import IngSoft.venta.bean.PersonaMiniBeanFuncion;
+import IngSoft.venta.bean.SocioBeanData;
+import IngSoft.venta.bean.SocioBeanFuncion;
+
+public class AccionConsultarSocio extends CoAccion{
+
+	@Override
+	public void ejecutar(ServletContext sc, HttpServletRequest request,
+			HttpServletResponse response)  throws CoException{
+		
+		
+		String codigo=request.getParameter("codigo");
+		
+		PersonaMiniBeanFuncion PersonaFuncion=PersonaMiniBeanFuncion.getInstanceP();
+		SocioBeanFuncion SocioFuncion= SocioBeanFuncion.getInstanceS();
+		
+		SocioBeanData SocioData=SocioFuncion.consultarSocio(codigo);
+		PersonaMiniBeanData PersonaData=PersonaFuncion.consultarPersona(codigo);
+		
+		request.setAttribute("socio", SocioData);
+		request.setAttribute("persona", PersonaData);
+
+		this.direccionar(sc, request, response, "/IngSoft/ventas/socio/consultarsocio.jsp");
+	}
+	
+}
