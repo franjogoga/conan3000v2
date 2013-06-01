4c54f49ebad25857f5cefadd30c24d4b7d033ff8
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-01 01:24:14 +0000

    DB_criterioproveedor

--- trunk/Conan3000V2/src/IngSoft/venta/bean/CriterioProveedorBeanData.java
+++ trunk/Conan3000V2/src/IngSoft/venta/bean/CriterioProveedorBeanData.java
@@ -3,46 +3,59 @@ package IngSoft.venta.bean;
 public class CriterioProveedorBeanData {
 	
 	private String codigo;
-	public String getCodigo() {
-		return codigo;
-	}
-	public void setCodigo(String codigo) {
-		this.codigo = codigo;
-	}
-	public String getRazon() {
-		return razon;
-	}
-	public void setRazon(String razon) {
-		this.razon = razon;
-	}
-	public String getRuc() {
-		return ruc;
-	}
-	public void setRuc(String ruc) {
-		this.ruc = ruc;
-	}
-	public String getDireccion() {
-		return direccion;
-	}
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
-	}
-	public String getCorreo() {
-		return correo;
-	}
-	public void setCorreo(String correo) {
-		this.correo = correo;
-	}
-	public String getContacto() {
-		return contacto;
-	}
-	public void setContacto(String contacto) {
-		this.contacto = contacto;
-	}
-	private String razon;
-	private String ruc;
+	private String razonSocial;
+	private long ruc;
 	private String direccion;
 	private String correo;
 	private String contacto;
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getRazonSocial() {
+		return razonSocial;
+	}
+
+	public void setRazonSocial(String razonSocial) {
+		this.razonSocial = razonSocial;
+	}
+
+	public long getRuc() {
+		return ruc;
+	}
+
+	public void setRuc(long ruc) {
+		this.ruc = ruc;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+	
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+	public String getCorreo() {
+		return correo;
+	}
+	
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	
+	public String getContacto() {
+		return contacto;
+	}
+	
+	public void setContacto(String contacto) {
+		this.contacto = contacto;
+	}
+	
 
 }
