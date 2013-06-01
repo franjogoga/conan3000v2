fbe724e192c4a246e789065995ff3e3f2d43edd7
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-01 21:53:14 +0000

    DB_accioneliminarproveedor

--- trunk/Conan3000V2/src/IngSoft/venta/proveedor/AccionEliminarProveedor.java
+++ trunk/Conan3000V2/src/IngSoft/venta/proveedor/AccionEliminarProveedor.java
@@ -1,5 +1,38 @@
 package IngSoft.venta.proveedor;
 
-public class AccionEliminarProveedor {
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import IngSoft.general.CoAccion;
+import IngSoft.general.CoException;
+import IngSoft.venta.bean.PromocionBeanData;
+import IngSoft.venta.bean.PromocionBeanFuncion;
+import IngSoft.venta.bean.ProveedorBeanData;
+import IngSoft.venta.bean.ProveedorBeanFuncion;
+
+public class AccionEliminarProveedor extends CoAccion{
+
+	@Override
+	public void ejecutar(ServletContext sc, HttpServletRequest request,
+			HttpServletResponse response) throws CoException {
+		// TODO Auto-generated method stub
+		
+		if(Integer.valueOf(request.getParameter("tipo"))==2){
+			
+			ProveedorBeanFuncion proveedorFuncion= ProveedorBeanFuncion.getInstanceS();
+			proveedorFuncion.eliminarProveedor(request.getParameter("codigo"));
+			this.direccionar(sc, request, response, "/IngSoft/ventas/proveedor/buscarproveedor.jsp");
+		}
+		if(Integer.valueOf(request.getParameter("tipo"))==1){
+		ProveedorBeanFuncion proveedorFuncion= ProveedorBeanFuncion.getInstanceS();
+		ProveedorBeanData proveedorData=proveedorFuncion.consultarProveedor(request.getParameter("codigo"));
+		
+		request.setAttribute("proveedor", proveedorData);
+		this.direccionar(sc, request, response, "/IngSoft/ventas/proveedor/eliminarproveedor.jsp");
+		}
+		//this.direccionar(sc, request, response, "/IngSoft/ventas/promociones/buscarpromocion.jsp");
+			
+	}
+	
 }
