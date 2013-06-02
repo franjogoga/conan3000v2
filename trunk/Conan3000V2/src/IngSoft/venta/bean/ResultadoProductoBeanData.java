56528d9f98f916a9ee587c9e1d729cabc19f8cff
lizita.diaz@gmail.com <lizita.diaz@gmail.com@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-02 04:35:16 +0000

    LD

---
+++ trunk/Conan3000V2/src/IngSoft/venta/bean/ResultadoProductoBeanData.java
@@ -0,0 +1,45 @@
+package IngSoft.venta.bean;
+
+public class ResultadoProductoBeanData {
+	
+	private String idProdProveedor;
+	
+	private String nombre;
+	private String presentacion;
+	private String descripcion;
+	private String precioU;
+
+	public String getIdProdProveedor() {
+		return idProdProveedor;
+	}
+	public void setIdProdProveedor(String idProdProveedor) {
+		this.idProdProveedor = idProdProveedor;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getPresentacion() {
+		return presentacion;
+	}
+	public void setPresentacion(String presentacion) {
+		this.presentacion = presentacion;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getPrecioU() {
+		return precioU;
+	}
+	public void setPrecioU(String precioU) {
+		this.precioU = precioU;
+	}
+
+
+}
