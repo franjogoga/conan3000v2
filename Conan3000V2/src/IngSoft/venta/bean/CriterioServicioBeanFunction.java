e9711a27c2851d691ea1121f05d677df894aa37c
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-06 18:41:11 +0000

    DB_criterioservicobeanfunction

---
+++ Conan3000V2/src/IngSoft/venta/bean/CriterioServicioBeanFunction.java
@@ -0,0 +1,47 @@
+package IngSoft.venta.bean;
+
+import java.sql.Date;
+import java.text.ParseException;
+import java.util.List;
+import java.util.Vector;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.ibatis.session.SqlSession;
+
+import IngSoft.general.MyBatisSesion;
+
+public class CriterioServicioBeanFunction {
+
+public CriterioServicioBeanData crearCriterio(HttpServletRequest request, HttpServletResponse response){
+		
+		CriterioServicioBeanData criterioServicioData= new CriterioServicioBeanData();
+		
+		//criterioPromocionData.setTipo(Integer.parseInt(request.getParameter("cmbTipoEvento")==null?"0":request.getParameter("cmbTipoEvento")));
+		criterioServicioData.setNombreServicio(request.getParameter("txtNombre")+"%");
+		criterioServicioData.setNombreProveedor(request.getParameter("txtProveedor")+"%");
+		//try {		
+			
+		//} 
+		//catch (ParseException e) {
+			//e.printStackTrace();
+		//}
+		return criterioServicioData;				
+	}
+	
+	public Vector<ResultadoServicioBeanData> buscarPlantillaServicio(CriterioServicioBeanData criterioServicioData){		
+		SqlSession sqlsesion=MyBatisSesion.metodo().openSession();
+		Vector<ResultadoServicioBeanData> resultadosV=null;
+		try{		
+		List<ResultadoServicioBeanData> resultados=sqlsesion.selectList("searchPlantillaServicio",criterioServicioData);
+						
+		resultadosV= new Vector<>(resultados);
+		}
+		finally{
+		sqlsesion.close();}
+		return resultadosV;
+		
+	}
+	
+}
