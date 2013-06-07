dfa0593cec95e8da639e5bc6c116b65da9b53f7f
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-07 04:39:49 +0000

    DB_accionconsultarservicio

---
+++ Conan3000V2/src/IngSoft/venta/servicio/AccionConsultarServicio.java
@@ -0,0 +1,32 @@
+package IngSoft.venta.servicio;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import IngSoft.general.CoAccion;
+import IngSoft.general.CoException;
+import IngSoft.venta.bean.ProveedorBeanData;
+import IngSoft.venta.bean.ServicioBeanData;
+import IngSoft.venta.bean.ServicioBeanFuncion;
+
+public class AccionConsultarServicio extends CoAccion{
+	
+	@Override
+	public void ejecutar(ServletContext sc, HttpServletRequest request,
+			HttpServletResponse response)  throws CoException{
+		
+		
+		String codigo=request.getParameter("codigo");
+
+		ServicioBeanFuncion ServicioFuncion= ServicioBeanFuncion.getInstanceS();
+		ServicioBeanData ServicioData=ServicioFuncion.consultarServicio(codigo);
+		ProveedorBeanData ProveedorData=ServicioFuncion.consultarProveedor(codigo);
+		
+		request.setAttribute("proveedor",ProveedorData);
+		request.setAttribute("servicio", ServicioData);
+
+		this.direccionar(sc, request, response, "/IngSoft/ventas/servicioproveedor/consultarservicio.jsp");
+	}
+
+}
