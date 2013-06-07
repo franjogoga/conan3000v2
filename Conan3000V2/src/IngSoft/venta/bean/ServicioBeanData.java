35b00018f0aad9f706f74dc4b7a150ba16b0af0c
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-07 04:39:08 +0000

    DB_serviciobeandata

---
+++ Conan3000V2/src/IngSoft/venta/bean/ServicioBeanData.java
@@ -0,0 +1,61 @@
+package IngSoft.venta.bean;
+
+import java.io.Serializable;
+
+public class ServicioBeanData implements Serializable {
+
+	private String codServicio;
+	private String codProveedor;
+	private String nombreServicio;
+	private String nombreProveedor;
+	private double precio;
+	private String descripcion;
+	private String estado;
+	public String getCodServicio() {
+		return codServicio;
+	}
+	public void setCodServicio(String codServicio) {
+		this.codServicio = codServicio;
+	}
+	public String getCodProveedor() {
+		return codProveedor;
+	}
+	public void setCodProveedor(String codProveedor) {
+		this.codProveedor = codProveedor;
+	}
+	public String getNombreServicio() {
+		return nombreServicio;
+	}
+	public void setNombreServicio(String nombreServicio) {
+		this.nombreServicio = nombreServicio;
+	}
+	public String getNombreProveedor() {
+		return nombreProveedor;
+	}
+	public void setNombreProveedor(String nombreProveedor) {
+		this.nombreProveedor = nombreProveedor;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
+	
+	
+	
+}
