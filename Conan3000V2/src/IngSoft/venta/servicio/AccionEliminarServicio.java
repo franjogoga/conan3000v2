9edd8d224b4fd6babd1025324f72b68a3ee94fb2
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-07 04:40:02 +0000

    DB_accioneliminarServicio

---
+++ Conan3000V2/src/IngSoft/venta/servicio/AccionEliminarServicio.java
@@ -0,0 +1,43 @@
+package IngSoft.venta.servicio;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import IngSoft.general.CoAccion;
+import IngSoft.general.CoException;
+import IngSoft.venta.bean.ProveedorBeanData;
+import IngSoft.venta.bean.ServicioBeanData;
+import IngSoft.venta.bean.ServicioBeanFuncion;
+
+
+public class AccionEliminarServicio extends CoAccion{
+
+	@Override
+	public void ejecutar(ServletContext sc, HttpServletRequest request,
+			HttpServletResponse response) throws CoException {
+		// TODO Auto-generated method stub
+		
+		ServicioBeanFuncion servicioFuncion= ServicioBeanFuncion.getInstanceS();
+		String codigo=request.getParameter("codigo");	
+		
+		if(Integer.valueOf(request.getParameter("tipo"))==2){
+
+			servicioFuncion.eliminarServicio(codigo);
+			
+			this.direccionar(sc, request, response, "/IngSoft/ventas/servicioproveedor/buscarservicio.jsp");
+		}
+		
+		if(Integer.valueOf(request.getParameter("tipo"))==1){
+		
+		ServicioBeanData servicioData=servicioFuncion.consultarServicio(codigo);
+		ProveedorBeanData proveedorData=servicioFuncion.consultarProveedor(codigo);
+		
+		request.setAttribute("proveedor", proveedorData);
+		request.setAttribute("servicio", servicioData);
+
+		this.direccionar(sc, request, response, "/IngSoft/ventas/servicioproveedor/eliminarservicio.jsp");
+		}		
+	}
+	
+}
