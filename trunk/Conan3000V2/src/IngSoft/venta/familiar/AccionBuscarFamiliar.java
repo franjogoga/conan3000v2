580eb8fec2779123c2a4adc6241e90815b6897d0
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-14 00:31:34 +0000

    DB_accionbuscarfamiliar

---
+++ trunk/Conan3000V2/src/IngSoft/venta/familiar/AccionBuscarFamiliar.java
@@ -0,0 +1,31 @@
+package IngSoft.venta.familiar;
+
+import java.util.Vector;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import IngSoft.general.CoAccion;
+import IngSoft.general.CoException;
+import IngSoft.venta.bean.CriterioFamiliarBeanData;
+import IngSoft.venta.bean.CriterioFamiliarBeanFuncion;
+import IngSoft.venta.bean.ResultadoFamiliarBeanData;
+
+public class AccionBuscarFamiliar extends CoAccion {
+	
+	@Override
+	public void ejecutar(ServletContext sc, HttpServletRequest request,
+			HttpServletResponse response) throws CoException {
+		// TODO Auto-generated method stub
+		
+		CriterioFamiliarBeanData criterioFamiliarData =new CriterioFamiliarBeanFuncion().crearCriterio(request, response);
+		Vector<ResultadoFamiliarBeanData> resultados=new CriterioFamiliarBeanFuncion().buscarPlantillaFamiliar(criterioFamiliarData);
+		
+		//request.setAttribute(name, o)
+		request.setAttribute("resultados", resultados);
+		this.direccionar(sc, request, response, "/IngSoft/ventas/familia/buscarfamiliar.jsp");
+		
+	}
+
+}
