041d6e9916f65ab5b4ca3bac9430db613e965be1
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-14 00:34:54 +0000

    DB_familiarbeandata

---
+++ Conan3000V2/src/IngSoft/venta/bean/FamiliarBeanData.java
@@ -0,0 +1,70 @@
+package IngSoft.venta.bean;
+
+import java.io.Serializable;
+
+public class FamiliarBeanData implements Serializable{
+	
+	private String codigoFamiliar;
+	private String codigoSocio;
+	private String parentesco;
+	private long telefonoFijo;
+	private long telefonoCelular;
+	private String correo;
+	private String estado;
+	
+	public String getCodigoFamiliar() {
+		return codigoFamiliar;
+	}
+	
+	public void setCodigoFamiliar(String codigoFamiliar) {
+		this.codigoFamiliar = codigoFamiliar;
+	}
+	
+	public String getCodigoSocio() {
+		return codigoSocio;
+	}
+	
+	public void setCodigoSocio(String codigoSocio) {
+		this.codigoSocio = codigoSocio;
+	}
+	
+	public String getParentesco() {
+		return parentesco;
+	}
+	
+	public void setParentesco(String parentesco) {
+		this.parentesco = parentesco;
+	}
+	
+	public long getTelefonoFijo() {
+		return telefonoFijo;
+	}
+	
+	public void setTelefonoFijo(long telefonoFijo) {
+		this.telefonoFijo = telefonoFijo;
+	}
+	
+	public long getTelefonoCelular() {
+		return telefonoCelular;
+	}
+	
+	public void setTelefonoCelular(long telefonoCelular) {
+		this.telefonoCelular = telefonoCelular;
+	}
+	
+	public String getCorreo() {
+		return correo;
+	}
+	
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	
+	public String getEstado() {
+		return estado;
+	}
+	
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+}
