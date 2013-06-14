f75810038a65a8f45a7f2a805328a67b0f59fce0
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-14 00:34:46 +0000

    DB_familiarbeanfuncion

---
+++ Conan3000V2/src/IngSoft/venta/bean/FamiliarBeanFuncion.java
@@ -0,0 +1,141 @@
+package IngSoft.venta.bean;
+
+import java.text.SimpleDateFormat;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.ibatis.session.SqlSession;
+
+import IngSoft.general.CoException;
+import IngSoft.general.MyBatisSesion;
+
+public class FamiliarBeanFuncion {
+
+	static private FamiliarBeanFuncion FamiliarFuncion=null;
+	private Lock l= new ReentrantLock();     
+	SimpleDateFormat DF = new SimpleDateFormat("dd/MM/yyyy");
+	   
+	   public static FamiliarBeanFuncion getInstance(){
+	       if(FamiliarFuncion==null) FamiliarFuncion= new FamiliarBeanFuncion();
+	       
+	       return FamiliarFuncion;
+	   }
+	   
+	   private FamiliarBeanFuncion() {}
+	
+	public FamiliarBeanData crearMembresia(HttpServletRequest request, HttpServletResponse response){
+		FamiliarBeanData familiarData= new FamiliarBeanData();
+		try{		
+	
+		//familiarData.setCodigoSocio(request.getParameter("idSocio"));
+		//familiarData.setFechaInicio(new Date(DF.parse(request.getParameter("fFechaInicio")).getTime()));
+		//familiarData.setFechafin(new Date(DF.parse(request.getParameter("fFechaFin")).getTime()));
+		//familiarData.setCosto(Double.parseDouble(request.getParameter("txtCosto")));
+		 	
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			
+		}
+		return familiarData;		
+	} 
+	
+	public boolean agregarMembresia(MembresiaBeanData membresiaData) throws CoException {
+		
+		boolean resultado=false;		
+		l.lock();
+		SqlSession sqlsesion=MyBatisSesion.metodo().openSession();
+		
+		try{
+			String codigo= (String)sqlsesion.selectOne("Data.venta.membresia.getNextCodigo2");
+			if(codigo!=null){
+			int cod= Integer.parseInt(codigo.substring(3))+1;
+			String defecto= "000000";
+			String temp= defecto.substring(0, defecto.length()-String.valueOf(cod).length()).concat(String.valueOf(cod));
+			
+			membresiaData.setCodigo(codigo.substring(0,3).concat(temp));}
+			else membresiaData.setCodigo("MO000001");
+			//insertMembresia esta en membresia mapper
+			sqlsesion.insert("insertMembresia",membresiaData);
+			//sqlsesion.insert("insertPlantillaEventoSedes",eventoData);
+			
+			resultado=true;
+		}
+		catch(Exception a)		
+		{sqlsesion.rollback();
+		a.printStackTrace();
+			//throw CoException.set("Error: Nombre de membresia repetido", "SMVMembresia?accion=Agregar&tipo=1");
+		}
+		
+		finally{
+			sqlsesion.commit();
+			sqlsesion.close();
+			l.unlock();					
+		}
+		return resultado;
+	}
+	
+	public boolean eliminarFamiliar(String codigo) throws CoException {
+		boolean resultado=false;		
+		
+		SqlSession sqlsesion=MyBatisSesion.metodo().openSession();
+		try{
+		
+			sqlsesion.update("Data.venta.familiar.deleteFamiliar",codigo);
+			
+			resultado=true;
+		}
+		catch(Exception a)		
+		{sqlsesion.rollback();
+		a.printStackTrace();
+			throw CoException.set("Error: No se pudo eliminar al familiar, intente de nuevo", "SMVFamiliar?accion=Agregar&tipo=1");
+			
+		}
+		
+		finally{
+			sqlsesion.commit();
+			sqlsesion.close();					
+		}
+			
+		return resultado;
+	}
+	
+	
+			
+	public FamiliarBeanData consultarFamiliar(String codigo){
+		FamiliarBeanData familiarData=null;
+		SqlSession sqlsesion=MyBatisSesion.metodo().openSession();
+		try{
+			familiarData= sqlsesion.selectOne("Data.venta.familiar.getPlantillaFamiliar",codigo);
+		}
+		finally{
+			sqlsesion.close();
+		}
+		return familiarData;
+	}
+
+	public void modificarMembresia(MembresiaBeanData membresia) throws CoException {
+		SqlSession sqlsesion=MyBatisSesion.metodo().openSession();
+		try{
+					
+		//	System.out.println("Ambs="+mods.size());
+			sqlsesion.update("Data.venta.membresia.updateMembresia",membresia);
+		}
+		catch(Exception a)		
+		{sqlsesion.rollback();
+		a.printStackTrace();
+			throw CoException.set("Error: No se pudo modificar la membresia, intente de nuevo", "SMVMembresia?accion=Modificar&tipo=1");
+			
+		}
+		
+		finally{
+			sqlsesion.commit();
+			sqlsesion.close();					
+		}			
+		return ;
+	}
+	
+}
