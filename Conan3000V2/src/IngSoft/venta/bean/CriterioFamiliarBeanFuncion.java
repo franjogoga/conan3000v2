e41b4bceaf27aec5c81b1e34365c50a5f9091b0f
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-14 00:35:10 +0000

    DB_criteriofamiliarbeanfuncion

---
+++ Conan3000V2/src/IngSoft/venta/bean/CriterioFamiliarBeanFuncion.java
@@ -0,0 +1,47 @@
+package IngSoft.venta.bean;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Vector;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.ibatis.session.SqlSession;
+
+import IngSoft.general.MyBatisSesion;
+
+public class CriterioFamiliarBeanFuncion {
+
+	SimpleDateFormat DF = new SimpleDateFormat("dd/MM/yyyy");
+
+	public CriterioFamiliarBeanData crearCriterio(HttpServletRequest request, HttpServletResponse response){
+		
+		CriterioFamiliarBeanData criterioFamiliarData= new CriterioFamiliarBeanData();
+		
+		//criterioMembresiaData.setTipo(Integer.parseInt(request.getParameter("cmbTipoEvento")==null?"0":request.getParameter("cmbTipoEvento")));
+			criterioFamiliarData.setNombres(request.getParameter("txtNombres")+"%");	
+			criterioFamiliarData.setApellidoPaterno(request.getParameter("txtApellidoPaterno")+"%");
+			criterioFamiliarData.setApellidoMaterno(request.getParameter("txtApellidoMaterno")+"%");
+			
+			//if (request.getParameter("rButton")!=null){
+			//String est;
+			//est=request.getParameter("rButton");
+			//criterioMembresiaData.setEstado(est);}
+		return criterioFamiliarData;				
+	}
+	
+	public Vector<ResultadoFamiliarBeanData> buscarPlantillaFamiliar(CriterioFamiliarBeanData criterioFamiliarData){		
+		SqlSession sqlsesion=MyBatisSesion.metodo().openSession();
+		Vector<ResultadoFamiliarBeanData> resultadosV=null;
+		try{		
+		List<ResultadoFamiliarBeanData> resultados=sqlsesion.selectList("searchPlantillaFamiliar",criterioFamiliarData);
+						
+		resultadosV= new Vector<>(resultados);
+		}
+		finally{
+		sqlsesion.close();}
+		return resultadosV;
+		
+	}
+}
