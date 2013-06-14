078371157751e089c7fa23adaff52f1fa5b903e6
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-14 05:21:31 +0000

    DB_accionagregarfamiliar

---
+++ trunk/Conan3000V2/src/IngSoft/venta/familiar/AccionAgregarFamiliar.java
@@ -0,0 +1,38 @@
+package IngSoft.venta.familiar;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import IngSoft.general.CoAccion;
+import IngSoft.general.CoException;
+import IngSoft.venta.bean.FamiliarBeanData;
+import IngSoft.venta.bean.FamiliarBeanFuncion;
+import IngSoft.venta.bean.PersonaMiniBeanData;
+import IngSoft.venta.bean.PersonaMiniBeanFuncion;
+
+public class AccionAgregarFamiliar extends CoAccion{
+
+	@Override
+	public void ejecutar(ServletContext sc, HttpServletRequest request,
+			HttpServletResponse response) throws CoException {
+		// TODO Auto-generated method stub
+	
+				int tipo=Integer.parseInt(request.getParameter("tipo"));
+				if(tipo==2){
+					FamiliarBeanFuncion familiarFuncion= FamiliarBeanFuncion.getInstance();
+					PersonaMiniBeanFuncion personaFuncion=PersonaMiniBeanFuncion.getInstanceP();
+					
+					FamiliarBeanData familiarData=familiarFuncion.crearFamiliar(request, response);
+					PersonaMiniBeanData personaData=personaFuncion.crearPersona(request, response);
+					
+					familiarFuncion.agregarPersona(personaData);
+					familiarFuncion.agregarFamiliar(familiarData);
+					this.direccionar(sc, request, response, "/IngSoft/ventas/familia/buscarfamiliar.jsp");
+				}
+				
+				this.direccionar(sc, request, response, "/IngSoft/ventas/familia/agregarfamiliar.jsp");
+		
+	}
+
+}
