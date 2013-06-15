dc5fa7ce00ad9febdd090dd3eb90c4561261d122
jonatan.gonzales@pucp.pe <jonatan.gonzales@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-15 01:31:27 +0000

    arreglo de suspender socio

--- Conan3000V2/src/IngSoft/administracion/bean/SocioBeanFunction.java
+++ Conan3000V2/src/IngSoft/administracion/bean/SocioBeanFunction.java
@@ -32,7 +32,7 @@ public class SocioBeanFunction {
 		SqlSession sesion = MyBatisSesion.metodo().openSession();
 		SocioBeanData datos = null;
 		try {
-			datos = sesion.selectOne("Data.administracion.socio.getSocio", codigo);			
+			datos = sesion.selectOne("Data.administracion.socio.getSocio", codigo);				
 			String strfechaInicio = datos.getFechaInicio().toString();											
 			strfechaInicio = strfechaInicio.replace("-", "");
 			int fechaInicio = Integer.parseInt(strfechaInicio);			
@@ -76,27 +76,27 @@ public class SocioBeanFunction {
 		SqlSession sesion = MyBatisSesion.metodo().openSession();
 		SocioBeanData datos = null;
 		try {
-			datos = sesion.selectOne("Data.administracion.socio.getSocio", codigo);					
-			SuspensionBeanData datosSuspension = new SuspensionBeanData();			
-			String codigoSuspension = (String)sesion.selectOne("Data.administracion.socio.getNextCodigoSuspension");			
-			if (codigoSuspension != null) {
-				int cod = Integer.parseInt(codigoSuspension.substring(3)) + 1;
-				String defecto = "000000";
-				
-				String temp = defecto.substring(0, defecto.length() - String.valueOf(cod).length()).concat(String.valueOf(cod));
-				datosSuspension.setIdSuspensionPago(codigoSuspension.substring(0,3).concat(temp));
+			datos = sesion.selectOne("Data.administracion.socio.getSocio", codigo);			
+			if (datos.getEstado().equalsIgnoreCase("Vencido")) {
+				SuspensionBeanData datosSuspension = new SuspensionBeanData();			
+				String codigoSuspension = (String)sesion.selectOne("Data.administracion.socio.getNextCodigoSuspension");			
+				if (codigoSuspension != null) {
+					int cod = Integer.parseInt(codigoSuspension.substring(3)) + 1;
+					String defecto = "000000";				
+					String temp = defecto.substring(0, defecto.length() - String.valueOf(cod).length()).concat(String.valueOf(cod));
+					datosSuspension.setIdSuspensionPago(codigoSuspension.substring(0,3).concat(temp));
+				}
+				else datosSuspension.setIdSuspensionPago("SUS000001");			
+				datosSuspension.setIdMembresia(datos.getCodigo().toString());
+				Calendar c = Calendar.getInstance();												
+				int dia = c.get(Calendar.DATE);		
+				int mes = 1+ c.get(Calendar.MONTH);
+				int annio = c.get(Calendar.YEAR);
+				String fechaRegistro = Integer.toString(annio) + "-" + Integer.toString(mes) + "-"+ Integer.toString(dia);			
+				datosSuspension.setFechaRegistro(fechaRegistro);
+				sesion.insert("Data.administracion.socio.insertSuspension", datosSuspension);	
+				sesion.update("Data.administracion.socio.updateSuspendidoSocio", codigo);
 			}
-			else datosSuspension.setIdSuspensionPago("SUS000001");
-			System.out.println("esto es  : "+ datosSuspension.getIdSuspensionPago());
-			datosSuspension.setIdMembresia(datos.getCodigo().toString());
-			Calendar c = Calendar.getInstance();												
-			int dia = c.get(Calendar.DATE);		
-			int mes = 1+ c.get(Calendar.MONTH);
-			int annio = c.get(Calendar.YEAR);
-			String fechaRegistro = Integer.toString(annio) + "-" + Integer.toString(mes) + "-"+ Integer.toString(dia);			
-			datosSuspension.setFechaRegistro(fechaRegistro);
-			sesion.insert("Data.administracion.socio.insertSuspension", datosSuspension);	
-			sesion.update("Data.administracion.socio.updateSuspendidoSocio", codigo);
 		} catch (Exception e3){
 			sesion.rollback();
 			e3.printStackTrace();
