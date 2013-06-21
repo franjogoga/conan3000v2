b8f51fc9d86e9899c8215e16e505d18377b4be46
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-21 03:44:54 +0000

    DB_resultadosolicitudsociobeandata

---
+++ trunk/Conan3000V2/src/IngSoft/venta/bean/ResultadoSolicitudSocioBeanData.java
@@ -0,0 +1,64 @@
+package IngSoft.venta.bean;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class ResultadoSolicitudSocioBeanData implements Serializable{
+	
+	private String codSolicitudSocio;
+	private String nombres;
+	private String apellidoPaterno;
+	private String apellidoMaterno;
+	private Date fechaRegistro;
+	private String estado;
+	
+	
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public Date getFechaRegistro() {
+		return fechaRegistro;
+	}
+
+	public void setFechaRegistro(Date fechaRegistro) {
+		this.fechaRegistro = fechaRegistro;
+	}
+
+	public String getCodSolicitudSocio() {
+		return codSolicitudSocio;
+	}
+	
+	public void setCodSolicitudSocio(String codSolicitudSocio) {
+		this.codSolicitudSocio = codSolicitudSocio;
+	}
+	
+	public String getNombres() {
+		return nombres;
+	}
+	
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	
+	public String getApellidoPaterno() {
+		return apellidoPaterno;
+	}
+	
+	public void setApellidoPaterno(String apellidoPaterno) {
+		this.apellidoPaterno = apellidoPaterno;
+	}
+	
+	public String getApellidoMaterno() {
+		return apellidoMaterno;
+	}
+	
+	public void setApellidoMaterno(String apellidoMaterno) {
+		this.apellidoMaterno = apellidoMaterno;
+	}
+
+}
