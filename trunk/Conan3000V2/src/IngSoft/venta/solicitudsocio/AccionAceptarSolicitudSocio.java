8cbcb4776ef1cd73ba29c74d221a66c4e1ec63cc
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-21 04:46:25 +0000

    DB_accionaceptarsolicitud

---
+++ trunk/Conan3000V2/src/IngSoft/venta/solicitudsocio/AccionAceptarSolicitudSocio.java
@@ -0,0 +1,33 @@
+package IngSoft.venta.solicitudsocio;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import IngSoft.general.CoAccion;
+import IngSoft.general.CoException;
+import IngSoft.venta.bean.SolicitudSocioBeanFuncion;
+import IngSoft.venta.bean.SolicitudsocioBeanData;
+
+public class AccionAceptarSolicitudSocio extends CoAccion{
+	
+	@Override
+	public void ejecutar(ServletContext sc, HttpServletRequest request,
+			HttpServletResponse response) throws CoException {
+		// TODO Auto-generated method stub
+		
+		String codigo=request.getParameter("codigo");
+		
+		SolicitudSocioBeanFuncion solicitudFuncion=SolicitudSocioBeanFuncion.getInstanceS();
+		SolicitudsocioBeanData solicitud=solicitudFuncion.consultarSolicitud(codigo);
+		
+		if(solicitud.getEstado().equalsIgnoreCase("Nueva")){
+			solicitud.setEstado("Aceptada");
+			solicitudFuncion.modificarSolicitud(solicitud);
+		}
+
+		this.direccionar(sc, request, response, "/IngSoft/ventas/solicitud/buscarsolicitudsocio.jsp");
+		
+	}
+
+}
