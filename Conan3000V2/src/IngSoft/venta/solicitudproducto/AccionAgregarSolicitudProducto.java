8a8dce0bd38e74adaaa0ef5d5340484abf82c05d
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-21 19:30:37 +0000

    DB_AccionAgregarSolicitudProducto

--- Conan3000V2/src/IngSoft/venta/solicitudproducto/AccionAgregarSolicitudProducto.java
+++ Conan3000V2/src/IngSoft/venta/solicitudproducto/AccionAgregarSolicitudProducto.java
@@ -7,10 +7,10 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import IngSoft.administracion.bean.SedeMiniBeanData;
+import IngSoft.servicio.bean.SedeMiniBeanData;
 import IngSoft.general.CoAccion;
 import IngSoft.general.CoException;
-import IngSoft.venta.bean.EmpleadoMiniBeanData;
+import IngSoft.servicio.bean.EventoBeanFuncion;
 import IngSoft.venta.bean.SolicitudProductoBeanData;
 import IngSoft.venta.bean.SolicitudProductoBeanFuncion;
 
@@ -22,6 +22,7 @@ public class AccionAgregarSolicitudProducto extends CoAccion {
 			HttpServletResponse response) throws CoException {
 		// TODO Auto-generated method stub
 				int tipo=Integer.parseInt(request.getParameter("tipo"));
+				EventoBeanFuncion eventoFunction= EventoBeanFuncion.getInstance();
 				if(tipo==2){
 					SolicitudProductoBeanFuncion solproductoFunction= SolicitudProductoBeanFuncion.getInstance();
 					SolicitudProductoBeanData solproductoData=solproductoFunction.crearSolProducto(request, response);
@@ -35,6 +36,9 @@ public class AccionAgregarSolicitudProducto extends CoAccion {
 					}
 				}
 				    if(tipo==1){
+				    	Vector<SedeMiniBeanData> sedeMiniData=eventoFunction.getSedes();
+
+						request.setAttribute("sedes",sedeMiniData );
 				    	
 				    	
 				        this.direccionar(sc, request, response, "/IngSoft/ventas/solicitud/agregarsolicitudproducto.jsp");
