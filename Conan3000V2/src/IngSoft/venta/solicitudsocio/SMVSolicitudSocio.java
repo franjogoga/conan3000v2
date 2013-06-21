177195a3f41ae5f2fd31f6b2d1bc8333f0b3e318
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-21 04:45:16 +0000

    DB_SMVsolicitudSocio

--- Conan3000V2/src/IngSoft/venta/solicitudsocio/SMVSolicitudSocio.java
+++ Conan3000V2/src/IngSoft/venta/solicitudsocio/SMVSolicitudSocio.java
@@ -8,9 +8,9 @@ public class SMVSolicitudSocio extends CoServlet{
 	@Override
 	protected void initAcciones() {
 		acciones.put("Buscar", new AccionBuscarSolicitudSocio());
-		//acciones.put("Consultar", new AccionConsultarSocio());
-		acciones.put("Agregar", new AccionAgregarSolicitudSocio()); //este es para agregar en traslado	
-		//acciones.put("Eliminar", new AccionEliminarSocio());
+		acciones.put("Agregar", new AccionAgregarSolicitudSocio());
+		acciones.put("Aceptar", new AccionAceptarSolicitudSocio());
+		acciones.put("Denegar", new AccionDenegarSolicitudSocio());
 	}
 
 }
