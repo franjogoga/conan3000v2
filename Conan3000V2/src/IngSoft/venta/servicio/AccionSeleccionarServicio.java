d1eaff3e188224f7cf1f46f1a99dc2aaefbbdf23
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-21 19:30:00 +0000

    DB_AccionSeleccionarServicio

---
+++ Conan3000V2/src/IngSoft/venta/servicio/AccionSeleccionarServicio.java
@@ -0,0 +1,30 @@
+package IngSoft.venta.servicio;
+
+import java.util.Vector;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import IngSoft.general.CoAccion;
+import IngSoft.general.CoException;
+import IngSoft.venta.bean.CriterioServicioBeanData;
+import IngSoft.venta.bean.CriterioServicioBeanFunction;
+import IngSoft.venta.bean.ResultadoServicioBeanData;
+
+public class AccionSeleccionarServicio extends CoAccion{
+
+	@Override
+	public void ejecutar(ServletContext sc, HttpServletRequest request,
+			HttpServletResponse response) throws CoException {
+		// TODO Auto-generated method stub
+		
+		CriterioServicioBeanData criterioServicioData =new CriterioServicioBeanFunction().crearCriterio(request,response);
+		Vector<ResultadoServicioBeanData> resultados=new CriterioServicioBeanFunction().buscarPlantillaServicio(criterioServicioData);
+		
+		//request.setAttribute(name, o)
+		request.setAttribute("resultados", resultados);
+		this.direccionar(sc, request, response, "/IngSoft/ventas/servicioproveedor/seleccionarservicio.jsp");
+		
+	}
+}
