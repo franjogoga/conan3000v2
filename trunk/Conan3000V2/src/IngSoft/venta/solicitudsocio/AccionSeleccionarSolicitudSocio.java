af2a1c7c3f8be079c92102ab5c5f763c8d96bcd2
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-22 09:44:49 +0000

    DB_AccionSeleccionarSolicitudSocio

---
+++ trunk/Conan3000V2/src/IngSoft/venta/solicitudsocio/AccionSeleccionarSolicitudSocio.java
@@ -0,0 +1,43 @@
+package IngSoft.venta.solicitudsocio;
+
+import java.util.Vector;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import IngSoft.general.CoAccion;
+import IngSoft.general.CoException;
+import IngSoft.venta.bean.CriterioSolicitudSocioBeanData;
+import IngSoft.venta.bean.CriterioSolicitudSocioBeanFuncion;
+import IngSoft.venta.bean.ResultadoSolicitudSocioBeanData;
+import IngSoft.venta.bean.SolicitudSocioBeanFuncion;
+import IngSoft.venta.bean.SolicitudsocioBeanData;
+
+public class AccionSeleccionarSolicitudSocio extends CoAccion{
+
+	@Override
+	public void ejecutar(ServletContext sc, HttpServletRequest request,
+			HttpServletResponse response) throws CoException {
+		// TODO Auto-generated method stub
+		
+		String codigo=request.getParameter("codigo");
+		
+		SolicitudSocioBeanFuncion solicitudFuncion=SolicitudSocioBeanFuncion.getInstanceS();
+		SolicitudsocioBeanData solicitud=solicitudFuncion.consultarSolicitud(codigo);
+		
+		if(solicitud.getEstado().equalsIgnoreCase("Aceptada")){
+			solicitud.setEstado("Registrada");
+			solicitudFuncion.modificarSolicitud(solicitud);
+		}
+		
+		CriterioSolicitudSocioBeanData criterioSolicitudData =new CriterioSolicitudSocioBeanFuncion().crearCriterio(request, response);
+		Vector<ResultadoSolicitudSocioBeanData> resultados=new CriterioSolicitudSocioBeanFuncion().buscarPlantillaSolicitudAceptada(criterioSolicitudData);
+		
+		//request.setAttribute(name, o)
+		request.setAttribute("resultados", resultados);
+		this.direccionar(sc, request, response, "/IngSoft/ventas/solicitud/seleccionarsolicitudsocio.jsp");
+		
+	}
+	
+}
