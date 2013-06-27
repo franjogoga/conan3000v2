5f5853a7eb187077e87adfea91745277426a482e
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-27 06:52:26 +0000

    DB_accionagregarsocio

--- Conan3000V2/src/IngSoft/venta/socio/AccionAgregarSocio.java
+++ Conan3000V2/src/IngSoft/venta/socio/AccionAgregarSocio.java
@@ -9,15 +9,10 @@ import javax.servlet.http.HttpServletResponse;
 import IngSoft.general.CoAccion;
 import IngSoft.general.CoException;
 import IngSoft.venta.bean.DistritoMiniBeanData;
-import IngSoft.venta.bean.MembresiaBeanData;
-import IngSoft.venta.bean.MembresiaBeanFunction;
 import IngSoft.venta.bean.PersonaMiniBeanData;
 import IngSoft.venta.bean.PersonaMiniBeanFuncion;
 import IngSoft.venta.bean.SocioBeanData;
 import IngSoft.venta.bean.SocioBeanFuncion;
-import IngSoft.venta.bean.TrasladoBeanData;
-import IngSoft.venta.bean.TrasladoBeanFuncion;
-
 
 public class AccionAgregarSocio extends CoAccion{
 	
@@ -25,48 +20,27 @@ public class AccionAgregarSocio extends CoAccion{
 	public void ejecutar(ServletContext sc, HttpServletRequest request,
 			HttpServletResponse response) throws CoException {
 		// TODO Auto-generated method stub
-				
-				
-				
+
 				int tipo=Integer.parseInt(request.getParameter("tipo"));
 				if(tipo==2){
-					
-					//TrasladoBeanFuncion trasladoFuncion= TrasladoBeanFuncion.getInstance();
-					//TrasladoBeanData trasladoData=trasladoFuncion.crearTraslado(request, response);
-					
-					
+
 					
 					SocioBeanFuncion socioFuncion= SocioBeanFuncion.getInstanceS();
 					SocioBeanData socioData=socioFuncion.crearSocio(request, response);
-					//socioData.setIdMembresia("MEM000032");
+
 					socioFuncion.agregarSocio(socioData);
 					
 					PersonaMiniBeanFuncion personaFuncion=PersonaMiniBeanFuncion.getInstanceP();
 					PersonaMiniBeanData personaData=personaFuncion.crearPersona(request, response);
 					personaFuncion.agregarPersona(personaData);
 					
-					MembresiaBeanFunction membresiaFuncion=MembresiaBeanFunction.getInstance();
-					//MembresiaBeanData membresiaData2=new MembresiaBeanData();
-					//membresiaData2.setIdMembresia(request.getParameter("txtIdMembresiaAntiguo"));
-					//membresiaFuncion.agregarMembresia(membresiaData);
-					
-					
-					//trasladoFuncion.agregarTraslado(trasladoData);
-				
-					//request.setAttribute("membresia2", membresiaData2);
 					request.setAttribute("socio", socioData);
 
 					this.direccionar(sc, request, response, "/IngSoft/ventas/membresia/trasladomembresia.jsp");
-					//request.setAttribute("typeahead7",codigo);
 				
 				}
 				SocioBeanFuncion socioFunction= SocioBeanFuncion.getInstanceS();
 				Vector<DistritoMiniBeanData> distritoMiniData=socioFunction.getDistritos();
-				//Vector<TipoEventoMiniBeanData> tipoEventoMiniData=eventoFunction.getTipoEvento();
-				//Vector<AmbienteMiniBeanData> AmbienteMiniData=eventoFunction.getAmbientes();
-				//request.setAttribute("sedes",sedeMiniData );
-				//request.setAttribute("ambientes",AmbienteMiniData );
-				
 				
 				request.setAttribute("tiposDistrito",distritoMiniData);
 				
