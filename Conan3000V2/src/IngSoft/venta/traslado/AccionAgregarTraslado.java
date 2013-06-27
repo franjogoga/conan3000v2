261e55d73a621e8760854bbacedf4f5740e12c57
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-27 05:34:13 +0000

    DB_accionagregartraslado

--- Conan3000V2/src/IngSoft/venta/traslado/AccionAgregarTraslado.java
+++ Conan3000V2/src/IngSoft/venta/traslado/AccionAgregarTraslado.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import IngSoft.general.CoAccion;
 import IngSoft.general.CoException;
+import IngSoft.venta.bean.OrdenPagoBeanFunction;
 import IngSoft.venta.bean.TrasladoBeanData;
 import IngSoft.venta.bean.TrasladoBeanFuncion;
 
@@ -21,6 +22,9 @@ public class AccionAgregarTraslado extends CoAccion {
 					TrasladoBeanData trasladoData=trasladoFuncion.crearTraslado(request, response);
 
 					trasladoFuncion.agregarTraslado(trasladoData);
+					
+					OrdenPagoBeanFunction orden=new OrdenPagoBeanFunction();
+					orden.agregarOrdenPago("TRASLADOMEMBRESIA", trasladoData.getIdmembresiantiguo(), trasladoData.getIdmembresianuevo(),trasladoData.getCodigo(),trasladoData.getMonto(), trasladoData.getFecha(), trasladoData.getFechafin());
 
 					//aca agregar el codigo para que se pase el codigo que se ha generado
 					
