81e36238b7b33f705c3e4aff6722d7f1c830ee78
daniel.bernal@pucp.pe <daniel.bernal@pucp.pe@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-28 07:08:22 +0000

    DB_accionagregarsolicitudservicio

--- Conan3000V2/src/IngSoft/venta/solicitudservicio/AccionAgregarSolicitudServicio.java
+++ Conan3000V2/src/IngSoft/venta/solicitudservicio/AccionAgregarSolicitudServicio.java
@@ -7,9 +7,9 @@ import javax.servlet.http.HttpServletResponse;
 import IngSoft.general.CoAccion;
 import IngSoft.general.CoException;
 import IngSoft.venta.bean.LineaServicioBeanData;
-import IngSoft.venta.bean.ProveedorBeanData;
 import IngSoft.venta.bean.ServicioBeanData;
 import IngSoft.venta.bean.ServicioBeanFuncion;
+import IngSoft.venta.bean.SolicitudServicioBeanData;
 
 public class AccionAgregarSolicitudServicio extends CoAccion{
 
@@ -23,15 +23,23 @@ public class AccionAgregarSolicitudServicio extends CoAccion{
 				//busco el servicio y saco su costo
 				ServicioBeanFuncion servicioFuncion=ServicioBeanFuncion.getInstanceS();
 				ServicioBeanData servicioData=servicioFuncion.consultarServicio(codServicio);
+				SolicitudServicioBeanData solicitudData=new SolicitudServicioBeanData();
 				
 				//Busco maxima codigo de solicitud de servicio
 				
 				String codSolicitudS=servicioFuncion.consultarSolicitudServicio();
+				if (codSolicitudS!=null){
+					int codSolicitud= Integer.parseInt(codSolicitudS.substring(3))+1;
+					String defecto= "000000";
+					String temp= defecto.substring(0, defecto.length()-String.valueOf(codSolicitud).length()).concat(String.valueOf(codSolicitud));
+				
+				solicitudData.setCodigoSolicitud(codSolicitudS.substring(0,3).concat(temp));}
+				else solicitudData.setCodigoSolicitud("SSR000001");	
 				
 				//creo instancia de linea de servicio
 				LineaServicioBeanData lineaData=new LineaServicioBeanData();
 				lineaData.setCodServicioP(codServicio);
-				lineaData.setCodSolicitudS(codSolicitudS);
+				lineaData.setCodSolicitudS(solicitudData.getCodigoSolicitud());
 				
 				//inserto la linea, aca llamo funcion y dentro de ella busco la ultima linea de servicio, luego hago operacion con monto por mes
 				//seteo el monto y inserto
@@ -46,14 +54,14 @@ public class AccionAgregarSolicitudServicio extends CoAccion{
 				else lineaData.setCodLineaServicio("LIS000001");
 				
 				Double precio=servicioData.getPrecio();//dice que saca null
-				int periodo=Integer.parseInt(request.getParameter("txtperiodo"));
+				double periodo=Double.parseDouble(request.getParameter("txtPeriodo"));
 				
 				Double monto=(double)precio*periodo;
 				lineaData.setMonto(monto);
 				
 				boolean bool=servicioFuncion.agregarLinea(lineaData);
 				
-				this.direccionar(sc, request, response, "/IngSoft/ventas/solicitud/buscarsolicitudservicio.jsp");
+				this.direccionar(sc, request, response, "/IngSoft/ventas/solicitud/agregarsolicitudservicio.jsp");
 	
 	}
 }
