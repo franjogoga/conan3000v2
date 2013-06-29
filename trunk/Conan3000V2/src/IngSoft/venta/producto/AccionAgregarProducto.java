81b5ae9957d347335ed4936246e2b462c36d6af9
lizita.diaz@gmail.com <lizita.diaz@gmail.com@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-06-29 22:24:51 +0000

    productoOK!!!_Liz Diaz

--- trunk/Conan3000V2/src/IngSoft/venta/producto/AccionAgregarProducto.java
+++ trunk/Conan3000V2/src/IngSoft/venta/producto/AccionAgregarProducto.java
@@ -10,6 +10,7 @@ import IngSoft.general.CoAccion;
 import IngSoft.general.CoException;
 import IngSoft.venta.bean.ProductoBeanData;
 import IngSoft.venta.bean.ProductoBeanFunction;
+import IngSoft.venta.bean.ProveedorBeanData;
 
 
 public class AccionAgregarProducto extends CoAccion {
@@ -23,11 +24,16 @@ public class AccionAgregarProducto extends CoAccion {
 				if(tipo==2){
 					ProductoBeanFunction productoFunction= ProductoBeanFunction.getInstance();
 					ProductoBeanData productoData=productoFunction.crearProducto(request, response);
+					String nombre=request.getParameter("txtProveedor");
+					ProveedorBeanData proveedorData=productoFunction.BuscarProveedor(nombre);
+					String codigo=proveedorData.getIdProveedor();
+					productoData.setIdProveedor(codigo);
+					//membresiaFunction.modificarSocio(membresiaData.getCodigoSocio());
 					productoFunction.agregarProducto(productoData);
 					response.setContentType("text/plain");  
 				    response.setCharacterEncoding("UTF-8");
 				    try {
-						response.getWriter().write( "/IngSoft/ventas/producto/SMVProducto?accion=Buscar&tipo=1");
+						response.getWriter().write("/IngSoft/ventas/producto/SMVProducto?accion=Buscar&tipo=1");
 					} catch (IOException e) {				
 						e.printStackTrace();
 					}
