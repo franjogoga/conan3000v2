adcf9d52f36f2d1d6e7dc734fe450dd9964fbebd
victor.avalos.91@gmail.com <victor.avalos.91@gmail.com@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-07-01 00:59:29 +0000

    VA_varios-Victor Avalos

--- trunk/Conan3000V2/src/Club/servicio/reserva/AccionCrearReserva.java
+++ trunk/Conan3000V2/src/Club/servicio/reserva/AccionCrearReserva.java
@@ -1,9 +1,6 @@
 package Club.servicio.reserva;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.Vector;
 
 import javax.servlet.ServletContext;
@@ -11,17 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.mysql.jdbc.Util;
-
 import IngSoft.general.CoAccion;
 import IngSoft.general.CoException;
-import IngSoft.servicio.bean.CriterioEventoBeanData;
-import IngSoft.servicio.bean.CriterioEventoBeanFunction;
 import IngSoft.servicio.bean.ReservaBeanFuncion;
-import IngSoft.servicio.bean.ReservaBungalowMiniBeanData;
-import IngSoft.servicio.bean.ResultadoEventoBeanData;
-import IngSoft.servicio.bean.SedeMiniBeanData;
-import IngSoft.servicio.bean.Utils;
 
 public class AccionCrearReserva extends CoAccion {
 
