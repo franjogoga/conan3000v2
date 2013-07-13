bf6e00fd77601d525ed0580e16b9e9b9700bd3a8
victor.avalos.91@gmail.com <victor.avalos.91@gmail.com@8bed286d-4ecf-b621-dd47-a4e1c96c0df7>
2013-07-13 15:21:37 +0000

    VAS_ ServAdicional - Victor Avalos

---
+++ trunk/Conan3000V2/src/IngSoft/servicio/servadicional/AccionBuscarServAdicional.java
@@ -0,0 +1,20 @@
+package IngSoft.servicio.servadicional;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import IngSoft.general.CoAccion;
+import IngSoft.general.CoException;
+
+public class AccionBuscarServAdicional extends CoAccion {
+
+	@Override
+	public void ejecutar(ServletContext sc, HttpServletRequest request,
+			HttpServletResponse response) throws CoException {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
---
+++ trunk/Conan3000V2/src/IngSoft/servicio/servadicional/AccionEliminarServAdicional.java
@@ -0,0 +1,23 @@
+package IngSoft.servicio.servadicional;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import IngSoft.general.CoAccion;
+import IngSoft.general.CoException;
+
+public class AccionEliminarServAdicional extends CoAccion {
+
+	@Override
+	public void ejecutar(ServletContext sc, HttpServletRequest request,
+			HttpServletResponse response) throws CoException {
+		int tipo=Integer.parseInt(request.getParameter("tipo"));
+		if(tipo==1){					
+		}
+		if(tipo==2){					
+		}
+
+	}
+
+}
